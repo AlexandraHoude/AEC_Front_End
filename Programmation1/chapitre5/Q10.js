06577e948e523c117eecf179e2a4096222db3eac
AlexandraHoude <a.houde-pelletier@hotmail.com>
2020-01-26 15:03:57 -0500

    prog numero 10-11 + bootstrap

--- Programmation1/chapitre5/Q10.js
+++ Programmation1/chapitre5/Q10.js
@@ -3,3 +3,22 @@ INFORMATIQUE/ADMINISTRATION/HISTOIRE/GÉNIE/GEOGRAPHIE/BUREAUTIQUE
 Mettre ces options dans un tableau, puis demander à un étudiant d’entrer un nom d’option. Vous lui répondez si OUI ou NON le collège offre cette option.*/
 
 
+var cours  = ["INFORMATIQUE", "ADMINISTRATION", "HISTOIRE", "GÉNIE", "GEOGRAPHIE", "BUREAUTIQUE"];
+
+var choixUsager = prompt("Entrez le cours que vous souhaitez prendre").toUpperCase();
+
+var verification = false;
+
+for (var i = 0; i < cours.length; i++) {
+    if (choixUsager === cours[i]) {
+        verification = true;
+     }
+}
+
+if (verification) {
+    document.write("Ce cours est disponible");
+} else {
+    document.write("Ce cours n'est pas disponible");
+}
+
+
--- Programmation1/chapitre5/Q11.js
+++ Programmation1/chapitre5/Q11.js
@@ -1,2 +1,24 @@
 /* C’est le temps du recensement. Nous aimerions connaître le nombre d’enfants que chaque famille possède et par la suite
 afficher un tableau de statistiques comme suit : Le nombre d’enfants est l’indice du tableau. */
+
+var tabEnfant = [0,0,0,0,0,0,0,0,0,0,0,0];
+var nbEnfant = 0;
+
+while (nbEnfant !== 666) {
+    nbEnfant = Number(prompt("Veuillez entrez un nombre d'enfant svp, pour quitter le sondage entrez 666"));
+    if (nbEnfant >= 0 && nbEnfant <= 10) {
+        tabEnfant[nbEnfant]++;
+    } else if (nbEnfant > 10) {
+        tabEnfant[11]++
+    }
+    console.log(tabEnfant)
+}
+
+for (var i = 0; i < tabEnfant.length; i++) {
+    if (i <= 10) {
+        document.write("nombre de famille ayant " + i + " enfant(s) : " + tabEnfant[i] + "<br>");
+    } else if (i > 10) {
+        document.write("nombre de famille ayant plus de 10 enfants : " + tabEnfant[i] + "<br>");
+    }
+
+}
