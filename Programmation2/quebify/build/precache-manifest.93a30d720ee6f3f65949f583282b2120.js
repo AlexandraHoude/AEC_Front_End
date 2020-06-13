2f2c895b8698295a6314ff7cc4e74286e3b5e96c
Alexandra <a.houde-pelletier@hotmail.com>
2020-06-13 10:25:45 -0400

    build push

---
+++ Programmation2/quebify/build/precache-manifest.93a30d720ee6f3f65949f583282b2120.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0e415b644f626e3da9deafbf6d7bf6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e88593449672b55cea9a",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "04db561f8b05498a8942",
+    "url": "./static/css/main.ac31339b.chunk.css"
+  },
+  {
+    "revision": "e88593449672b55cea9a",
+    "url": "./static/js/2.971e3d95.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.971e3d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04db561f8b05498a8942",
+    "url": "./static/js/main.20778d6f.chunk.js"
+  },
+  {
+    "revision": "7df9ea4c741b1c205711",
+    "url": "./static/js/runtime-main.5323c564.js"
+  },
+  {
+    "revision": "b3360ff0acff1ca01f1b29e1eeacd46e",
+    "url": "./static/media/alexisonfire.b3360ff0.jpg"
+  },
+  {
+    "revision": "3018e0fadcf07157d2011d5eb81ff8c9",
+    "url": "./static/media/atreyu.3018e0fa.jpg"
+  },
+  {
+    "revision": "4300531adc4bf1b9270d09abf0ef11a7",
+    "url": "./static/media/deftones.4300531a.jpg"
+  },
+  {
+    "revision": "902f693dff9230e675f90e6056231c70",
+    "url": "./static/media/inthismoment.902f693d.png"
+  },
+  {
+    "revision": "6da7aa2aea34203788b4a3c8660e41dd",
+    "url": "./static/media/manson.6da7aa2a.png"
+  },
+  {
+    "revision": "a224a2c866ee4c0f0e31fa9fc59ce4c6",
+    "url": "./static/media/slipknot.a224a2c8.jpg"
+  },
+  {
+    "revision": "d9e24de269a606a87511858a82478d7b",
+    "url": "./static/media/spotify.d9e24de2.png"
+  }
+]);
