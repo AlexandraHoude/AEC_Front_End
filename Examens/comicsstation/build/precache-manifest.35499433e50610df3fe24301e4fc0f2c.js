6190f0b96f7fe8f5211d4f2bb3210b41cea9d759
AlexandraHoude <a.houde-pelletier@hotmail.com>
2020-08-29 10:07:23 -0400

    Build

---
+++ Examens/comicsstation/build/precache-manifest.35499433e50610df3fe24301e4fc0f2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1412d28cf49e2bd3a8665372692a42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5216ce36d8394be60d45",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9f4e9a980731c264b424",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5216ce36d8394be60d45",
+    "url": "./static/js/2.bc66658b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc66658b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4e9a980731c264b424",
+    "url": "./static/js/main.a1b7b999.chunk.js"
+  },
+  {
+    "revision": "521fe258087753b08490",
+    "url": "./static/js/runtime-main.65a5a3b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
